37292d21943758a6cdb271a98e0240b6cdc2c1bd
r45r54r45 <r45r54r45@yonsei.ac.kr>
2016-11-29 08:23:01 +0900

    Dd

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 function query(queryString, param){
     return new Promise(function(resolve, reject){
         var mysql=require('mysql');
+        // var connection = mysql.createConnection({
+        //     host     : 'localhost',
+        //     user     : 'root',
+        //     password : '',
+        //     database: 'db'
+        // });
         var connection = mysql.createConnection({
-            host     : 'localhost',
+            host     : '104.198.26.254',
             user     : 'root',
             password : '',
             database: 'db'
         });
+
         connection.connect();
         if(param){
             connection.query(queryString, param,function(err, rows, fields) {
